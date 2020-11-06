8025071718c672fad5713ad9b69d8651f4ce9f72
Javier Wamba <javierw81@gmail.com>
2020-11-06 12:35:35 +0100

    add typescript

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 function App() {
     return (
-        <div>Hello Javier!</div>
+        <div>Hello Javier and connie!</div>
     );
 }
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,41 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.tsx',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'index_bundle.js'
     },
+    target: "web",
+    resolve: {
+        extensions: [".js", ".jsx", ".json", ".ts", ".tsx"],
+    },
     module: {
-        rules: [
-            { 
-                test: /\.(js)$/, 
-                use: 'babel-loader' 
-            },
+        rules: [           
             { 
                 test: /\.css$/, 
                 use: ['style-loader', 'css-loader'] 
+            },
+            {
+                test: /\.(ts|tsx)$/,
+                loader: "awesome-typescript-loader",
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader",
             }
         ]
     },
     mode: 'development',
     plugins: [
-
         new HtmlWebpackPlugin({
             template: 'src/index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: "./src/index.css",
         })
     ]
 }
