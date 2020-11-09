32108eaa0a56dd42bc0b1fba5d38ef83e3aa902f
Javier Wamba <javierw81@gmail.com>
2020-11-09 18:30:14 +0100

    tests con jest y enzyme

---
+++ tests/components/button.test.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react'
+import { mount } from 'enzyme'
+import { theme } from '../mocks/theme'
+import { ThemeProvider } from 'styled-components'
+import Button from '../../src/components/Button'
+
+describe('Button', () => {
+
+  it('Button render is success', () => {
+    const wrapper = mount(
+      <ThemeProvider theme={theme}>
+        <Button>Hello Button</Button>
+      </ThemeProvider>,
+    );
+    expect(wrapper.find('button').length).toBe(1);
+  });
+
+  it('Button onClick is success', () => {
+    const clickFn = jest.fn();
+
+    const wrapper = mount(
+      <ThemeProvider theme={theme}>
+        <Button onClick={clickFn} >Hello Button</Button>
+      </ThemeProvider>,
+    )
+
+    wrapper
+      .find('button')
+      .at(0)
+      .simulate('click');
+
+    expect(clickFn).toHaveBeenCalled();
+  })
+})
---
+++ tests/mocks/theme.ts
@@ -0,0 +1,9 @@
+export const theme = {
+    color: '#FFFFFF',
+    primary: {
+        color: '#365b87'
+    },
+    secondary: {
+        color: '#CCCCCC'
+    }
+}
